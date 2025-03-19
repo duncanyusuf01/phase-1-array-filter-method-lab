37fb77d7356155a3931b13f87a0477914931aefb
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-03-19 16:47:14 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers=["Bobby","Sammy","Sally","Annette","Sarah","Bobby"];
+function findMatching(drivers, name){
+    return drivers.filter(drivers=>drivers.toLowerCase()=== name.toLowerCase());
+}
+function fuzzyMatch(drivers, name){
+    return drivers.filter(drivers=>drivers.toLowerCase().startsWith(name.toLowerCase()));
+}
+function matchName(drivers,name){
+    return drivers.filter(drivers=>drivers.name===name)
+}
